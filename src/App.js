4b6db2d06461f69c2333ba5f5a2d004f50ff6810
nafise amiri <nafisamiri1313@gmail.com>
2021-04-27 14:35:59 +0430

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import TodoList from './components/TodoList';
+
+function App () {
+  return (
+    <div className="todo-app">
+     <TodoList />
+    </div>
+  
+  );
+}
+
+      
+ 
+
+export default App
